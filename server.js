833c8712505bf573f8b4524270aa5ed23f8aae1e
Misqke <misqke.rust@yahoo.com>
2022-08-05 19:50:57 -0400

    fix cors

--- server.js
+++ server.js
@@ -12,14 +12,7 @@ const submitsRouter = require("./routes/submits");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const corsOptions = {
-  origin: [
-    "https://milkmanager-demo.netlify.app/",
-    "https://milkmanager.netlify.app/",
-  ],
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use("/api/milks", getMilkRouter);
 app.use("/api/submits", submitsRouter);
