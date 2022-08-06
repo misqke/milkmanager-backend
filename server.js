6d0d9da3ef405e62a1439aafe954a76b9e8d04d2
Misqke <misqke.rust@yahoo.com>
2022-08-05 20:46:55 -0400

    endpoint to start server

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 
 const app = express();
 
-// routes
 const getMilkRouter = require("./routes/getMilks");
 const submitsRouter = require("./routes/submits");
 
-// middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
-
+app.use("/api/start", (req, res) => res.status(200).json(true));
 app.use("/api/milks", getMilkRouter);
 app.use("/api/submits", submitsRouter);
 
